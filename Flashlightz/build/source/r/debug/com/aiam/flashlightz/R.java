7c639b180e2b0505084d6c66b7dd5276bdac5b78
Phelipe Maia <phelipemaia@gmail.com>
2013-06-30 12:10:48 -0300

    Adjust on adMob size

--- Flashlightz/build/source/r/debug/com/aiam/flashlightz/R.java
+++ Flashlightz/build/source/r/debug/com/aiam/flashlightz/R.java
@@ -13,8 +13,9 @@ public final class R {
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
-        public static final int button=0x7f050002;
-        public static final int buttonBottom=0x7f050003;
+        public static final int adWidth=0x7f050002;
+        public static final int button=0x7f050003;
+        public static final int buttonBottom=0x7f050004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
